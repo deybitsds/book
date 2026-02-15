4ce67fb665a851b76da9bbe01b2cae786e899e63
WSL-Ana <deybitsds1@gmail.com>
2026-02-15 15:42:53 -0500

    deleted buttons

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -56,33 +56,6 @@ export const UI = () => {
         >
           <img className="w-20" src="/images/wawasensei-white.png" />
         </a>
-        <div className="w-full overflow-auto pointer-events-auto flex justify-center">
-          <div className="overflow-auto flex items-center gap-4 max-w-full p-10">
-            {[...pages].map((_, index) => (
-              <button
-                key={index}
-                className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                  index === page
-                    ? "bg-white/90 text-black"
-                    : "bg-black/30 text-white"
-                }`}
-                onClick={() => setPage(index)}
-              >
-                {index === 0 ? "Cover" : `Page ${index}`}
-              </button>
-            ))}
-            <button
-              className={`border-transparent hover:border-white transition-all duration-300  px-4 py-3 rounded-full  text-lg uppercase shrink-0 border ${
-                page === pages.length
-                  ? "bg-white/90 text-black"
-                  : "bg-black/30 text-white"
-              }`}
-              onClick={() => setPage(pages.length)}
-            >
-              Back Cover
-            </button>
-          </div>
-        </div>
       </main>
 
       <div className="fixed inset-0 flex items-center -rotate-2 select-none">
